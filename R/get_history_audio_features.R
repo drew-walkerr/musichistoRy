35fcaf9128d23d7c01b1e003fa2f5d26ccc274e2
Drew <Drew@macbook-pro.lan>
2020-12-06 16:40:11 -0500

    moved important variables

--- R/get_history_audio_features.R
+++ R/get_history_audio_features.R
@@ -1,4 +1,8 @@
 get_history_audio_features <- function(my_data,user){
+  Sys.setenv(SPOTIFY_CLIENT_ID = '2c46a5d6764f425ab746a56a1c8791b9')
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = '9b809cd5be004e8fbbc72ad74b0e19a7')
+  access_token <- spotifyr::get_spotify_access_token(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
+                                                     client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"))
   track_audio_features <- function(artist, title, type = "track") {
   search_results <- spotifyr::search_spotify(paste(artist, title), type = type)
   track_audio_feats <- spotifyr::get_track_audio_features(search_results$id[[1]])
@@ -6,10 +10,6 @@ get_history_audio_features <- function(my_data,user){
 }
   # create a version of this function which can handle errors
   possible_af <- purrr::possibly(track_audio_features, otherwise = tidyr::tibble())
-  Sys.setenv(SPOTIFY_CLIENT_ID = '2c46a5d6764f425ab746a56a1c8791b9')
-  Sys.setenv(SPOTIFY_CLIENT_SECRET = '9b809cd5be004e8fbbc72ad74b0e19a7')
-  access_token <- spotifyr::get_spotify_access_token(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
-                                           client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"))
   colnames(my_data)
   colnames(my_data)[colnames(my_data)=="song_title"] = "title"
   tictoc::tic()
